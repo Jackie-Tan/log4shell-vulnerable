2afa71c16bbb170bfd522016d6fb2c0ea96e251b
Tho Nguyen <tho.nguyen@vnetwork.vn>
2023-09-01 14:52:56 +0700

    chore: initial project

---
+++ src/main/java/com/vnetwork/org/demo/MainController.java
@@ -0,0 +1,26 @@
+package com.vnetwork.org.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@RestController
+public class MainController {
+
+    private static final Logger logger = LogManager.getLogger("Hello");
+
+    @GetMapping("/")
+    public String index(@RequestHeader("X-Api-Version") String apiVersion) {
+        logger.info("Received a request for API version " + apiVersion);
+        return "Hello, world!";
+    }
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
+    }
+}
